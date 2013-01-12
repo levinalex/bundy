5e016c311308b7c76191a23da6469c65a77d4c8b
Levin Alexander <mail@levinalex.net>
2013-01-12 13:11:47 +0100

    initial import

---
+++ app/models/law.rb
@@ -0,0 +1,3 @@
+class Law < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bundy::Application.config.secret_token = 'f20e4df7c4362d85fd3c5dd7707f61e94b56f8dd23a30ffd2ba66628c23a3be696976126ec075edc2e8a8aac36ebdaa183ed8ee957b7c4b6640fb500e522a12d'
